e1a8dcfc4bf6047e54ae6d1a5231f12f7bff73c5
Krish Rajput <krishrajput1776cosmic@gmail.com>
2026-05-27 20:34:02 +0530

    added basic implementation

--- file_45.py
+++ file_45.py
@@ -5,3 +5,17 @@ def factorial(n):
         return 1
     return n * factorial(n-1)
 
+
+# 2026-05-27 20:34:02.879987
+def binary_search(arr, target):
+    low, high = 0, len(arr)-1
+    while low <= high:
+        mid = (low+high)//2
+        if arr[mid] == target:
+            return mid
+        elif arr[mid] < target:
+            low = mid+1
+        else:
+            high = mid-1
+    return -1
+
